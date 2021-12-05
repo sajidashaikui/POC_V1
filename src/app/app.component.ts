2f414fbaa02acbd949c13a263d92a1542a58d2a6
sajshaik <sajida.shaik@fiserv.com>
2021-12-05 16:25:09 +0530

    poc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
-
+const ELEMENT_DATA = [
+  { imageUrl: 'assets/download.jpg', band: 1, name: 'Matt', lastonline: '8 mins ago', country: 'US', battery:'19%' },
+  { imageUrl: 'assets/download.jpg', band: 2, name: 'Lisa', lastonline: '8 mins ago', country: 'UK', battery:'19%' },
+  { imageUrl: 'assets/download.jpg', band: 3, name: 'Emma', lastonline: '18 mins ago', country: 'US', battery:'19%' },
+  { imageUrl: 'assets/download.jpg', band: 4, name: 'Thomas', lastonline:'38 mins ago', country: 'US', battery:'19%' },
+  { imageUrl: 'assets/download.jpg', band: 5, name: 'Jill', lastonline: '30 mins ago', country: 'US' , battery:'19%'},
+  { imageUrl: 'assets/download.jpg', band: 6, name: 'Zach', lastonline: '30 mins ago', country: 'US' , battery:'19%'},
+  { imageUrl: 'assets/download.jpg', band: 7, name: 'Ron', lastonline: '40 mins ago', country: 'US', battery:'19%' },
+  { imageUrl: 'assets/download.jpg', band: 8, name: 'Chris', lastonline: '50 mins ago', country: 'US', battery:'19%' },
+  { imageUrl: 'assets/download.jpg', band: 9, name: 'Gordon', lastonline: '20 mins ago', country: 'US', battery:'19%' },
+  { imageUrl: 'assets/download.jpg', band: 10, name: 'Markus', lastonline: '55 mins ago', country: 'NUS' , battery:'19%'},
+];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'poc1';
+  displayedColumns: string[] = ['name', 'band', 'battery', 'lastonline', 'country'];
+  dataSource = ELEMENT_DATA;
+  title = 'Offenders';
+  longText = "Running";
+
+  servers = [{
+    name: "Server 1",
+    status: "Active",
+    state: "Running..",
+    desc : "Applications"
+  },
+  {
+    name: "Server 2",
+    status: "Active",
+    state: "Inactive.",
+    desc : "Applications"
+  },
+  {
+    name: "Server 3",
+    status: "Deactive",
+    state: "Inactive.",
+    desc : "Applications"
+  }
+];
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatTableModule} from '@angular/material/table';
+import {MatToolbarModule} from '@angular/material/toolbar';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+        /** Material Modules */
+        MatToolbarModule,
+        MatButtonModule,
+        MatDividerModule,
+        MatIconModule,
+        MatListModule,
+        MatCardModule,
+        MatSidenavModule,
+        MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
